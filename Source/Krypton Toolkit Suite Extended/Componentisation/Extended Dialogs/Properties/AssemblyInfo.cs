ec16851de70dc31257fb3abce3b59cf60fcb4e9d
Peter Wagner <peterwagner@live.co.uk>
2019-12-31 15:00:04 +0000

    * Files

--- Source/Krypton Toolkit Suite Extended/Componentisation/Extended Dialogs/Properties/AssemblyInfo.cs
+++ Source/Krypton Toolkit Suite Extended/Componentisation/Extended Dialogs/Properties/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.480.736.0")]
-[assembly: AssemblyFileVersion("5.480.736.0")]
+[assembly: AssemblyVersion("5.480.750.0")]
+[assembly: AssemblyFileVersion("5.480.750.0")]
--- Source/Krypton Toolkit Suite Extended/Full Toolkit/Extended File Dialogs/Controls/HelperTypes.cs
+++ Source/Krypton Toolkit Suite Extended/Full Toolkit/Extended File Dialogs/Controls/HelperTypes.cs
@@ -613,7 +613,7 @@ namespace ExtendedFileDialogs.Controls
         LeftToolBar = 0x4A0,
         ComboFileName = 0x47c,
         ComboFileType = 0x480,
-        ComboFolder = 0x471,
+        ComboFolder = 0x480,
         CheckBoxReadOnly = 0x410
     }
     #endregion
